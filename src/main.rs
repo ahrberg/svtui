9fb5827e3d5479348583e68f531b2038324dfe0a
ahrberg <ahrberg@kth.se>
2023-04-02 19:14:34 +0200

    simple but working svt client and main program
    run main program with page number as argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::collections::HashMap;
+use std::env;
+
+mod svt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    let args: Vec<String> = env::args().collect();
+
+    let page = match args.len() {
+        1 => &args[1],
+        2 => &args[1],
+        _ => panic!("Too many params!"),
+    };
+
+    let client = svt::SvtClient::new(String::from("https://www.svt.se/text-tv/api"));
+    let result = client.get_page(String::from(page)).await;
+    let resp = match result {
+        Ok(result) => result,
+        Err(error) => panic!("Error getting page: {:?}", error),
+    };
+
+    for p in resp.data.sub_pages {
+        println!("{}", p.alt_text)
+    }
     Ok(())
 }
---
+++ src/svt.rs
@@ -0,0 +1,79 @@
+use reqwest::{Client, Error};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct SvtResponse {
+    pub status: String,
+    pub data: Data,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Data {
+    #[serde(rename = "pageNumber")]
+    pub page_number: String,
+    #[serde(rename = "prevPage")]
+    pub prev_page: String,
+    #[serde(rename = "nextPage")]
+    pub next_page: String,
+    #[serde(rename = "subPages")]
+    pub sub_pages: Vec<SubPage>,
+    pub meta: Meta,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Meta {
+    pub updated: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SubPage {
+    #[serde(rename = "subPageNumber")]
+    pub sub_page_number: String,
+    #[serde(rename = "gifAsBase64")]
+    pub gif_as_base64: String,
+    #[serde(rename = "imageMap")]
+    pub image_map: String,
+    #[serde(rename = "altText")]
+    pub alt_text: String,
+}
+
+pub struct SvtClient {
+    base_url: String,
+    http_client: Client,
+}
+
+impl SvtClient {
+    pub fn new(base_url: String) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+        }
+    }
+    pub async fn get_page(&self, page: String) -> Result<SvtResponse, Error> {
+        let response = self
+            .http_client
+            .get(format!("{}/{}", self.base_url, page))
+            .send()
+            .await?;
+        let response: SvtResponse = response.json().await?;
+        Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[tokio::test]
+    async fn get_page() {
+        let client = SvtClient::new(String::from("https://www.svt.se/text-tv/api"));
+        let result = client.get_page(String::from("100")).await;
+        let response = match result {
+            Ok(result) => result,
+            Err(error) => panic!("Did not expect error: {:?}", error),
+        };
+
+        assert_eq!(response.data.page_number, "100")
+    }
+}
